2062b1de6bc83a044f4c282510a8f0c134defffa
Julian <julian.foerster@mavt.ethz.ch>
2019-11-11 20:57:15 +0100

    Added blackboard communication

--- mopa_demos/panda_grasp_demo/src/execution/behaviour_tree.py
+++ mopa_demos/panda_grasp_demo/src/execution/behaviour_tree.py
@@ -3,14 +3,29 @@ import py_trees_ros
 
 from panda_grasp_demo.msg import ScanSceneAction, ScanSceneGoal
 
+import std_msgs
 
 def get_root():
     # For a sketch of the tree layout, see here (slide 2): https://docs.google.com/presentation/d/1swC5c1mbVn2TRDar-y0meTbrC9BUHnT9XWYPeFJlNxM/edit#slide=id.g70bc070381_0_32
 
-    root = py_trees.composites.Selector()
+    # -------- Add nodes writing to blackboard -----------------------------------------
+
+    bb_root = py_trees.composites.Sequence()
+
+    grasp2bb = py_trees_ros.subscribers.ToBlackboard(name="grasp2bb",
+                                                     topic_name="/panda_demo/grasp_pose",
+                                                     topic_type=std_msgs.msg.String,
+                                                     blackboard_variables="grasp_pose",
+                                                     initialise_variables=None
+                                                    )
+    bb_root.add_child(grasp2bb)
+
+    # -------- Add nodes with condition checks and actions -----------------------------
+
+    action_root = py_trees.composites.Selector()
 
     check_obj_in_ws = py_trees.behaviours.Success(name="Object in workspace?")
-    root.add_child(py_trees.decorators.Inverter(check_obj_in_ws))
+    action_root.add_child(py_trees.decorators.Inverter(check_obj_in_ws))
 
     check_obj_in_hand = py_trees.behaviours.Failure(name="Object in hand?")
 
@@ -18,8 +33,8 @@ def get_root():
 
     scan_goal = ScanSceneGoal()
     scan_goal.num_scan_poses = 2
-    # action_get_grasp = py_trees.behaviours.SuccessEveryN(name="action_get_grasp", n=4)
-    action_get_grasp = py_trees.behaviours.Running(name="action_get_grasp")
+    # action_get_grasp = py_trees.behaviours.SuccessEveryN(name="Action compute grasp", n=4)
+    action_get_grasp = py_trees.behaviours.Running(name="Action compute grasp")
     # action_get_grasp = py_trees_ros.actions.ActionClient(name="Action compute grasp",
     #                                                      action_spec=ScanSceneAction,
     #                                                      action_goal=scan_goal,
@@ -36,7 +51,12 @@ def get_root():
     action_drop = py_trees.behaviours.Failure(name="Action drop object")
     composite_drop = py_trees.composites.Sequence(children=[composite_check_in_hand, action_drop])
 
-    root.add_child(composite_drop)
+    action_root.add_child(composite_drop)
+
+
+    # -------- Return root -----------------------------------------
+    root = py_trees.composites.Parallel(children=[bb_root, action_root])
+
     return root
 
 class PandaTree:
--- mopa_demos/panda_grasp_demo/src/run.py
+++ mopa_demos/panda_grasp_demo/src/run.py
@@ -4,18 +4,22 @@ import rospy
 
 from execution.behaviour_tree import PandaTree
 
+DEBUG = False
+PRINT_TREE = False
 
 def main():
     rospy.init_node("behaviour_tree_node")
 
-    pt = PandaTree(debug=False)
+    pt = PandaTree(debug=DEBUG)
     
     index = 1
+    rospy.loginfo("Starting loop...")
     while not rospy.is_shutdown():
-        print("\n--------- Tick {0} ---------\n".format(index))
+        if PRINT_TREE:
+            print("\n--------- Tick {0} ---------\n".format(index))
         pt.tree.tick()
-        print("\n")
-        py_trees.display.print_ascii_tree(pt.tree.root, show_status=True)
+        if PRINT_TREE:
+            py_trees.display.print_ascii_tree(pt.tree.root, show_status=True)
         index += 1
         time.sleep(1.0)   # Not really needed. Just for debugging.
 
