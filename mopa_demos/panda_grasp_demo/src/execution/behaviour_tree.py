64ff5397d2ba9e4481c341f93e57a92f35b63ed3
Julian <julian.foerster@mavt.ethz.ch>
2019-11-11 17:02:06 +0100

    First draft of tree

--- mopa_demos/panda_grasp_demo/src/execution/behaviour_tree.py
+++ mopa_demos/panda_grasp_demo/src/execution/behaviour_tree.py
@@ -5,5 +5,22 @@ import py_trees_ros
 def create_tree():
     root = py_trees.composites.Selector()
 
-    # obj_in_ws_check = py_trees.
+    check_obj_in_ws = py_trees.behaviours.Failure(name="Object in workspace?")
+    root.add_child(py_trees.decorators.Inverter(check_obj_in_ws))
 
+    check_obj_in_hand = py_trees.behaviours.Failure(name="Object in hand?")
+
+    check_grasp_pose_known = py_trees.behaviours.Failure(name="Grasp pose known?")
+    action_get_grasp = py_trees.behaviours.Failure(name="Action compute grasp")
+    composite_compute_grasp = py_trees.composites.Selector(children=[check_grasp_pose_known, action_get_grasp])
+
+    action_grasp = py_trees.behaviours.Failure(name="Action do grasp")
+    composite_do_grasp = py_trees.composites.Sequence(children=[composite_compute_grasp, action_grasp])
+
+    composite_check_in_hand = py_trees.composites.Selector(children=[check_obj_in_hand, composite_do_grasp])
+
+    action_drop = py_trees.behaviours.Failure(name="Action drop object")
+    composite_drop = py_trees.composites.Sequence(children=[composite_check_in_hand, action_drop])
+
+    root.add_child(composite_drop)
+    return root
