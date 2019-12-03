ad9f38b0e3653a7823c9422b2619b1a24c6165dd
Franka Box <ethz.asl.panda@gmail.com>
2019-12-03 23:10:52 +0100

    Downsample point cloud

--- moma_demos/grasp_demo/scripts/reconstruct_point_cloud.py
+++ moma_demos/grasp_demo/scripts/reconstruct_point_cloud.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import random
+import time
+
 import rospy
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs.point_cloud2 import read_points, create_cloud
@@ -12,6 +15,8 @@ class PointCloudReconstruction(object):
     def __init__(self):
         self.base_frame = "panda_link0"
         self.sensor_topic_name = "/camera/depth/color/points"
+        self.max_num_points = 1280 * 720
+
         self.running = False
         self.count = 0
 
@@ -32,12 +37,8 @@ class PointCloudReconstruction(object):
             "~point_cloud", PointCloud2, queue_size=10
         )
 
-        rospy.sleep(1.0)
-        self.reset(TriggerRequest())
-        self.start(TriggerRequest())
-
     def reset(self, request):
-        self.point_cloud = None
+        self.points = None
 
     def start(self, request):
         self.running = True
@@ -45,42 +46,36 @@ class PointCloudReconstruction(object):
     def stop(self, request):
         self.running = False
 
-    def sensor_cb(self, point_cloud):
+    def sensor_cb(self, point_cloud_msg):
         self.count += 1
-        if not self.running or self.count % 60 != 0:
+        if not self.running or self.count % 30 != 0:
             return
 
-        rospy.loginfo("Processing point cloud")
+        header = point_cloud_msg.header
+        fields = point_cloud_msg.fields
 
         # Lookup robot pose
-        camera_frame = point_cloud.header.frame_id
-        stamp = point_cloud.header.stamp
         T_robot_camera = self.tf_buffer.lookup_transform(
-            self.base_frame, camera_frame, stamp, rospy.Duration(0.1)
+            self.base_frame, header.frame_id, header.stamp, rospy.Duration(0.1)
         )
 
-        # TODO(mbreyer) sometimes an ExtrapolationException is raised if no robot pose with the current time can
-        # be found, probably it would be good to increase the publish rate of the robot state
-
         # Transform point cloud
-        transformed_point_cloud = do_transform_cloud(point_cloud, T_robot_camera)
-        transformed_point_cloud.header = point_cloud.header
-        transformed_point_cloud.header.frame_id = self.base_frame
+        point_cloud_msg = do_transform_cloud(point_cloud_msg, T_robot_camera)
+        point_cloud_msg.header = header
+        point_cloud_msg.header.frame_id = self.base_frame
 
         # Stitch point cloud
-        if self.point_cloud is None:
-            self.point_cloud = transformed_point_cloud
+        if self.points is None:
+            self.points = list(read_points(point_cloud_msg))
         else:
-            header = self.point_cloud.header
-            fields = self.point_cloud.fields
-            points_out = list(read_points(self.point_cloud)) + list(
-                read_points(transformed_point_cloud)
-            )
-            self.point_cloud = create_cloud(header, fields, points_out)
+            self.points += list(read_points(point_cloud_msg))
 
-        # Publish new point cloud
-        print("Hi")
-        self.point_cloud_pub.publish(self.point_cloud)
+        # Downsample by randomly selecting points
+        num_points = min(len(self.points), self.max_num_points)
+        self.points = random.sample(self.points, num_points)
+
+        # Publish stitched point cloud
+        self.point_cloud_pub.publish(create_cloud(header, fields, self.points))
 
 
 if __name__ == "__main__":
