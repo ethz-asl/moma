1408f3242b719f8463e04de2620c88f8f3bdf32b
Kenneth Blomqvist <kekeblom@gmail.com>
2019-12-12 12:14:13 +0100

    Scaffold dynamic buttons

--- moma_demos/grasp_demo/include/button_panel.h
+++ moma_demos/grasp_demo/include/button_panel.h
@@ -6,22 +6,34 @@
 #include <QVBoxLayout>
 #include <QPushButton>
 #include <QWidget>
+#include "grasp_demo/Instances.h"
 
 namespace control_gui {
 
 class ButtonPanel : public rviz::Panel {
     public:
-    ButtonPanel(QWidget *parent = 0); 
+    ButtonPanel(QWidget *parent = 0);
 
     public Q_SLOTS:
-    void onScanButtonClicked(); 
+    void onScanButtonClicked();
     void onGraspButtonClicked();
     void onStowButtonClicked();
     void onResetButtonClicked();
+    void instancesCallback(const grasp_demo::Instances &msg);
+    void setButtons();
+    void onInstanceGraspButtonClicked();
+
     private:
     ros::NodeHandle node_handle;
     ros::Publisher scan_publisher, grasp_publisher, stow_publisher, reset_publisher;
+    ros::Subscriber instance_subscriber;
+    QVBoxLayout *layout;
+    QPushButton *scan_button;
+    QPushButton *grasp_button;
+    QPushButton *stow_button;
+    QPushButton *reset_button;
+    std::vector<QPushButton*> instance_buttons;
 };
-} 
+}
 #endif // BUTTON_PANEL_H
 
--- moma_demos/grasp_demo/src/button_panel.cpp
+++ moma_demos/grasp_demo/src/button_panel.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+#include <string>
 #include "std_msgs/Empty.h"
 #include "button_panel.h"
 
@@ -8,13 +10,14 @@ ButtonPanel::ButtonPanel(QWidget *parent) : rviz::Panel(parent), node_handle("ma
     grasp_publisher = node_handle.advertise<std_msgs::Empty>("grasp", 1000);
     stow_publisher = node_handle.advertise<std_msgs::Empty>("stow", 1000);
     reset_publisher = node_handle.advertise<std_msgs::Empty>("reset", 1000);
+    instance_subscriber = node_handle.subscribe("/grasp_selection/instances", 1, &ButtonPanel::instancesCallback, this);
 
     // Setup Panel.
-    QVBoxLayout *layout = new QVBoxLayout;
-    QPushButton *scan_button = new QPushButton("Scan scene and plan grasp");
-    QPushButton *grasp_button = new QPushButton("Grasp object");
-    QPushButton *stow_button = new QPushButton("Stow object");
-    QPushButton *reset_button = new QPushButton("Reset object");
+    layout = new QVBoxLayout;
+    scan_button = new QPushButton("Scan scene and plan grasp");
+    grasp_button = new QPushButton("Grasp object");
+    stow_button = new QPushButton("Stow object");
+    reset_button = new QPushButton("Reset object");
     layout->addWidget(scan_button);
     layout->addWidget(grasp_button);
     layout->addWidget(stow_button);
@@ -25,6 +28,8 @@ ButtonPanel::ButtonPanel(QWidget *parent) : rviz::Panel(parent), node_handle("ma
     connect(grasp_button, &QPushButton::clicked, this, &ButtonPanel::onGraspButtonClicked);
     connect(stow_button, &QPushButton::clicked, this, &ButtonPanel::onStowButtonClicked);
     connect(reset_button, &QPushButton::clicked, this, &ButtonPanel::onResetButtonClicked);
+
+    setButtons();
 }
 
 void ButtonPanel::onScanButtonClicked() {
@@ -49,6 +54,50 @@ void ButtonPanel::onResetButtonClicked() {
     ROS_INFO("Reset object button was clicked");
     std_msgs::Empty msg;
     reset_publisher.publish(msg);
+}
+
+void ButtonPanel::instancesCallback(const grasp_demo::Instances &msg) {
+    layout->removeWidget(grasp_button);
+    layout->removeWidget(stow_button);
+    layout->removeWidget(reset_button);
+
+    std::vector<const char*> labels = {"one", "two", "three" };
+    for (int i = 0; i < 3; i++) {
+        QPushButton *button = new QPushButton(labels[i]);
+        layout->addWidget(button);
+    }
+    layout->addWidget(stow_button);
+    layout->addWidget(reset_button);
+}
+
+void ButtonPanel::setButtons() {
+    layout->removeWidget(grasp_button);
+    layout->removeWidget(stow_button);
+    layout->removeWidget(reset_button);
+    while (!instance_buttons.empty()) {
+        QPushButton *button = instance_buttons.back();
+        instance_buttons.pop_back();
+        layout->removeWidget(button);
+        disconnect(button, &QPushButton::clicked, this, &ButtonPanel::onInstanceGraspButtonClicked);
+        delete button;
+    }
+
+    std::vector<const char*> labels = {"one", "two", "three" };
+    for (int i = 0; i < 3; i++) {
+        //std::sstream stream;
+        //stream << "Grasp " << labels[i];
+        QPushButton *button = new QPushButton(labels[i]);
+        instance_buttons.push_back(button);
+        layout->addWidget(button);
+        connect(button, &QPushButton::clicked, this, &ButtonPanel::onInstanceGraspButtonClicked);
+    }
+    layout->addWidget(stow_button);
+    layout->addWidget(reset_button);
+}
+
+void ButtonPanel::onInstanceGraspButtonClicked() {
+    ROS_INFO("button clicked");
+    setButtons();
 }
 
 }
