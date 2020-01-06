713221c2d6b9b0ee9094661668825d34bf0cc7a1
Kenneth Blomqvist <kekeblom@gmail.com>
2020-01-06 19:58:21 +0100

    Recover semantic button panel

--- moma_demos/grasp_demo/include/semantic_button_panel.h
+++ moma_demos/grasp_demo/include/semantic_button_panel.h
@@ -11,29 +11,39 @@
 namespace control_gui
 {
 
+class InstancePushButton : public QPushButton {
+    private:
+    int instance_id;
+    ros::Publisher *topic_pub;
+
+    public:
+    InstancePushButton(const char *label, int instance_id, ros::Publisher *topic_pub);
+    ~InstancePushButton();
+    public Q_SLOTS:
+    void onButtonClick();
+};
+
 class SemanticButtonPanel : public rviz::Panel {
     public:
     SemanticButtonPanel(QWidget *parent = 0);
+    ~SemanticButtonPanel();
 
     public Q_SLOTS:
     void onScanButtonClicked();
-    void onGraspButtonClicked();
     void onStowButtonClicked();
     void onResetButtonClicked();
     void instancesCallback(const grasp_demo::Instances &msg);
-    void setButtons();
-    void onInstanceGraspButtonClicked();
 
     private:
     ros::NodeHandle node_handle;
-    ros::Publisher scan_publisher, grasp_publisher, stow_publisher, reset_publisher;
+    ros::Publisher scan_publisher, stow_publisher, reset_publisher;
     ros::Subscriber instance_subscriber;
+    ros::Publisher instance_publisher;
     QVBoxLayout *layout;
     QPushButton *scan_button;
-    QPushButton *grasp_button;
     QPushButton *stow_button;
     QPushButton *reset_button;
-    std::vector<QPushButton*> instance_buttons;
+    std::vector<InstancePushButton*> instance_buttons;
 };
 }
 #endif // SEMANTIC_BUTTON_PANEL_H
--- moma_demos/grasp_demo/src/semantic_button_panel.cpp
+++ moma_demos/grasp_demo/src/semantic_button_panel.cpp
@@ -2,80 +2,112 @@
 #include <string>
 #include "std_msgs/Empty.h"
 #include "semantic_button_panel.h"
+#include "grasp_demo/Instance.h"
+namespace control_gui { InstancePushButton::InstancePushButton(const char *label, int id, ros::Publisher *publisher) : QPushButton(label) {
+    instance_id = id;
+    topic_pub = publisher;
+    connect(this, &InstancePushButton::clicked, this, &InstancePushButton::onButtonClick);
+}
 
-namespace control_gui
-{
+InstancePushButton::~InstancePushButton() {
+    disconnect(this, &InstancePushButton::clicked, this, &InstancePushButton::onButtonClick);
+}
+
+void InstancePushButton::onButtonClick() {
+    ROS_INFO_STREAM("Instance " << instance_id << " button clicked");
+    grasp_demo::Instance instance_msg;
+    instance_msg.id = instance_id;
+    topic_pub->publish(instance_msg);
+}
 
 SemanticButtonPanel::SemanticButtonPanel(QWidget *parent) : rviz::Panel(parent), node_handle("manipulation_actions")
 {
-    reset_publisher = node_handle.advertise<std_msgs::Empty>("reset", 1000);
+    scan_publisher = node_handle.advertise<std_msgs::Empty>("scan", 1);
+    stow_publisher = node_handle.advertise<std_msgs::Empty>("stow", 1);
+    reset_publisher = node_handle.advertise<std_msgs::Empty>("reset", 1);
     instance_subscriber = node_handle.subscribe("/grasp_selection/instances", 1, &SemanticButtonPanel::instancesCallback, this);
+
+    instance_publisher = node_handle.advertise<grasp_demo::Instance>("instance_selected", 1);
 
     // Setup Panel.
     layout = new QVBoxLayout;
     scan_button = new QPushButton("Scan scene and plan grasp");
-    grasp_button = new QPushButton("Grasp object");
     stow_button = new QPushButton("Stow object");
     reset_button = new QPushButton("Reset object");
     layout->addWidget(scan_button);
-    layout->addWidget(grasp_button);
     layout->addWidget(stow_button);
     layout->addWidget(reset_button);
     setLayout(layout);
 
+    for (int i=0; i < 2; i++) {
+        //int instance_id = msg.instance_ids[i];
+        //std::string category = msg.semantic_categories[i];
+        std::string category = "Book";
+        int instance_id = i;
+        std::stringstream label_stream;
+        label_stream << "Grasp " << category;
+        std::string label = label_stream.str();
+
+        ROS_INFO_STREAM("" << label.c_str());
+        InstancePushButton *button = new InstancePushButton(label.c_str(), instance_id, &instance_publisher);
+        layout->addWidget(button);
+        instance_buttons.push_back(button);
+    }
+
+    connect(scan_button, &QPushButton::clicked, this, &SemanticButtonPanel::onScanButtonClicked);
+    connect(stow_button, &QPushButton::clicked, this, &SemanticButtonPanel::onStowButtonClicked);
     connect(reset_button, &QPushButton::clicked, this, &SemanticButtonPanel::onResetButtonClicked);
-    setButtons();
 }
 
-void SemanticButtonPanel::onResetButtonClicked()
-{
+SemanticButtonPanel::~SemanticButtonPanel() {
+    for (InstancePushButton *button : instance_buttons) {
+        delete button;
+    }
+}
+
+void SemanticButtonPanel::onScanButtonClicked() {
+    ROS_INFO("Scan button was clicked");
+    std_msgs::Empty msg;
+    scan_publisher.publish(msg);
+}
+
+void SemanticButtonPanel::onStowButtonClicked() {
+    ROS_INFO("Stow button was clicked");
+    std_msgs::Empty msg;
+    stow_publisher.publish(msg);
+}
+
+void SemanticButtonPanel::onResetButtonClicked() {
     ROS_INFO("Reset object button was clicked");
     std_msgs::Empty msg;
     reset_publisher.publish(msg);
 }
 
 void SemanticButtonPanel::instancesCallback(const grasp_demo::Instances &msg) {
-    layout->removeWidget(grasp_button);
     layout->removeWidget(stow_button);
     layout->removeWidget(reset_button);
 
-    std::vector<const char*> labels = {"one", "two", "three" };
-    for (int i = 0; i < 3; i++) {
-        QPushButton *button = new QPushButton(labels[i]);
-        layout->addWidget(button);
-    }
-    layout->addWidget(stow_button);
-    layout->addWidget(reset_button);
-}
-
-void SemanticButtonPanel::setButtons() {
-    layout->removeWidget(grasp_button);
-    layout->removeWidget(stow_button);
-    layout->removeWidget(reset_button);
     while (!instance_buttons.empty()) {
-        QPushButton *button = instance_buttons.back();
+        InstancePushButton *button = instance_buttons.back();
         instance_buttons.pop_back();
         layout->removeWidget(button);
-        disconnect(button, &QPushButton::clicked, this, &SemanticButtonPanel::onInstanceGraspButtonClicked);
         delete button;
     }
 
-    std::vector<const char*> labels = {"one", "two", "three" };
-    for (int i = 0; i < 3; i++) {
-        //std::sstream stream;
-        //stream << "Grasp " << labels[i];
-        QPushButton *button = new QPushButton(labels[i]);
-        instance_buttons.push_back(button);
+    for (int i=0; i < msg.instance_ids.size(); i++) {
+        int instance_id = msg.instance_ids[i];
+        std::string category = msg.semantic_categories[i];
+
+        std::stringstream label_stream;
+        label_stream << "Grasp " << category;
+        std::string label = label_stream.str();
+
+        InstancePushButton *button = new InstancePushButton(label.c_str(), instance_id, &instance_publisher);
         layout->addWidget(button);
-        connect(button, &QPushButton::clicked, this, &SemanticButtonPanel::onInstanceGraspButtonClicked);
+        instance_buttons.push_back(button);
     }
     layout->addWidget(stow_button);
     layout->addWidget(reset_button);
-}
-
-void SemanticButtonPanel::onInstanceGraspButtonClicked() {
-    ROS_INFO("button clicked");
-    setButtons();
 }
 
 }
