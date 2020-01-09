2d6e33eb3fb35fb82f35d498358631a557a6264f
Julien Kindle <jkindle@student.ethz.ch>
2020-01-09 14:08:51 +0100

    Added initial position fixing to veltopos

--- moma_gazebo/scripts/vel_to_pos_controller_node.py
+++ moma_gazebo/scripts/vel_to_pos_controller_node.py
@@ -3,11 +3,13 @@ import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
 import numpy as np
+import re
 
 
 class VelToPosControllerNode:
     def __init__(self):
         # Load parameters
+        self.init_config = rospy.get_param('~init_joint_config', None)
         self.rate = rospy.get_param('~rate', 125.0)
         default_joint_names = ["panda_joint{}".format(x) for x in range(1, 8)]
         self.joints = rospy.get_param('~joints', default_joint_names)
@@ -23,11 +25,22 @@ class VelToPosControllerNode:
                     idx_msg = msg.name.index(joint)
                     self.positions[idx] = msg.position[idx_msg]
             rospy.sleep(1.0)
+        if self.init_config is not None:
+            matches = re.findall(r"-J .*?(\w.*?)\s.*?([-+]?\d*\.\d+|\d+)",
+                                 self.init_config)
+            for name, value in matches:
+                if name in self.joints:
+                    print(name)
+                    idx = self.joints.index(name)
+                    self.positions[idx] = float(value)
 
         # Set up subscriptions and publishers
         self.sub_vel = rospy.Subscriber('~command_vel_in',
                                         Float64MultiArray,
                                         self.cb_joint_vels, queue_size=1)
+        self.sub_pos = rospy.Subscriber('~command_pos_in',
+                                        Float64MultiArray,
+                                        self.cb_joint_pos, queue_size=1)
         self.pub_pos = rospy.Publisher('~command_pos_out', Float64MultiArray,
                                        queue_size=1)
 
@@ -52,6 +65,15 @@ class VelToPosControllerNode:
                                                      data.shape))
             return
         self.velocities = data
+
+    def cb_joint_pos(self, msg):
+        data = np.array(msg.data)
+        if data.shape != self.positions.shape:
+            rospy.logerr("Received wrong position command. Expected shape "
+                         + "{}, received {}.".format(self.positions.shape,
+                                                     data.shape))
+            return
+        self.positions = data
 
 
 if __name__ == "__main__":
