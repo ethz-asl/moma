4a713b80d3d000a1615c50f286dbaa785569262b
Kenneth Blomqvist <kekeblom@gmail.com>
2020-01-14 13:48:17 +0100

    Playbook for deploying ridgeback container

--- ridgeback_docker/build_container.sh
+++ ridgeback_docker/build_container.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
 set -e
-docker build -t mobmi .
-docker save -o ../image.zip mobmi
+RIDGEBACK_DOCKER_DIR=$(dirname $(readlink -f "$0"))
+docker build -t mobmi $RIDGEBACK_DOCKER_DIR
+IMAGE_PATH=/tmp/mobmi_image.zip
+docker save -o $IMAGE_PATH mobmi
+
+echo "Saved container image to $IMAGE_PATH"
 
