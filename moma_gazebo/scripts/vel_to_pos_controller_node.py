ab989c5f004e0d09e4a7e77934e81d8bb05ca731
Julien Kindle <jkindle@student.ethz.ch>
2020-01-20 10:35:39 +0100

    Added service for setting joint states. Helpful since gazebo init config buggy (issue #93)

--- moma_gazebo/scripts/vel_to_pos_controller_node.py
+++ moma_gazebo/scripts/vel_to_pos_controller_node.py
@@ -2,6 +2,7 @@
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
+from moma_gazebo.srv import SetJoints, SetJointsResponse
 import numpy as np
 import re
 
@@ -44,9 +45,45 @@ class VelToPosControllerNode:
         self.pub_pos = rospy.Publisher('~command_pos_out', Float64MultiArray,
                                        queue_size=1)
 
+        # Set up service for changing arm configuration
+        self.srv_j = rospy.Service('~set_joint_states', SetJoints,
+                                   self.cb_set_joints)
+
         # Start timer for main loop
         self._timer = rospy.Timer(rospy.Duration(1/self.rate),
                                   self.publisher_loop)
+
+    def cb_set_joints(self, req):
+        resp = SetJointsResponse()
+        if not req.states or len(req.states.data) != self.n_joints:
+            resp.success = False
+            return resp
+        self.positions = req.states.data
+        resp.success = True
+
+        self.wait_for_joints()
+        return resp
+
+    def wait_for_joints(self):
+        self.real_pos = np.full(self.n_joints, np.nan)
+        sub_js = rospy.Subscriber('/joint_states',
+                                  JointState,
+                                  self.cb_joint_states, queue_size=1)
+        while not rospy.is_shutdown():
+            if not np.isnan(self.real_pos).any():
+                err = np.abs(self.real_pos - self.positions)
+                if (err <= 1e-3).all():
+                    break
+            rospy.sleep(0.25)
+
+        sub_js.unregister()
+        return
+
+    def cb_joint_states(self, msg):
+        for idx, joint in enumerate(self.joints):
+            if joint in msg.name:
+                idx_msg = msg.name.index(joint)
+                self.real_pos[idx] = msg.position[idx_msg]
 
     def publisher_loop(self, event):
         # Update positions
