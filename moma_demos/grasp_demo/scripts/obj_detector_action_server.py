dc7f04b6a7bf1484b57ad88da9ea2dc7b1e605b7
ZinnerC <cassian.a.zinner@gmail.com>
2020-05-24 21:26:01 +0200

    detection server cleaned up

--- moma_demos/grasp_demo/scripts/obj_detector_action_server.py
+++ moma_demos/grasp_demo/scripts/obj_detector_action_server.py
@@ -7,23 +7,16 @@ from std_msgs.msg import (
     String,
     UInt8
     )
-# from grasp_demo.msg import (
-#     BoundingBoxes,
-#     BoundingBox,
-#     DetectionAction,
-#     DetectionActionFeedback,
-#     DetectionActionGoal,
-#     DetectionActionResult,
-#     DetectionFeedback,
-#     DetectionGoal,
-#     DetectionResult
-#     )
-
-from grasp_demo.msg import *
 
 from actionlib import SimpleActionServer
 
-from grasp_demo.msg import DetectionAction, DetectionResult
+from grasp_demo.msg import (grasp_demo,
+    BoundingBox,
+    BoundingBoxes,
+    DetectionAction,
+    DetectionResult,
+    DetectionFeedback,
+)
 from grasp_demo.utils import create_robot_connection
 
 from cv_bridge import CvBridge, CvBridgeError
@@ -35,7 +28,7 @@ class yolo_action(object):
     _feedback = grasp_demo.msg.DetectionFeedback()
     _result = grasp_demo.msg.DetectionResult()
 
-    # Variables
+    # Variables for the neural network
     confThreshold = 0.5                #Confidence threshold
     nmsThreshold = 0.4                 #Non-maximum suppression threshold
     inpWidth = 416                     #Width of network's input image
@@ -60,11 +53,7 @@ class yolo_action(object):
     cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
 
     def __init__(self,name):
-        self.robot_name = sys.argv[1]
-        # self._load_parameters()
-        # self._connect_robot()
-
-        # self._action_name = name
+        # Setup action server
         self._as = SimpleActionServer(
             "detection_action", DetectionAction, execute_cb=self.BBcheck_cb, auto_start = False
         )
@@ -74,21 +63,16 @@ class yolo_action(object):
         self.boundingBox = None
         self.boundingBoxes = BoundingBoxes()
 
+        # Publisher
+        self.image_pub = rospy.Publisher("/detection_action/image",Image, queue_size=1)
+
         # Subscriber
-        # self.image_sub = rospy.Subscriber("/webcam/image_raw",Image,self.callback_IMG, queue_size=1)
         self.image_sub = rospy.Subscriber("/fixed_camera/color/image_raw",Image,self.callback_IMG, queue_size=1)
 
+        # Build neural network
         self.net = cv2.dnn.readNetFromDarknet(self.modelConfiguration, self.modelWeights)
         self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
-    # def _connect_robot(self):
-    #     full_robot_name = (
-    #         self.robot_name + "_" + self._robot_arm_names[0]
-    #         if len(self._robot_arm_names) > 1
-    #         else self.robot_name
-    #     )
-    #     self._robot_arm = create_robot_connection(full_robot_name)
 
     # Get the names of the output layers
     def getOutputsNames(self,net):
@@ -177,29 +161,27 @@ class yolo_action(object):
     def yolo_detector(self):
         self.boundingBoxes.bounding_box = []
         # Create a 4D blob from a frame.
-        # rospy.loginfo('yolo_detector 1')
         blob = cv2.dnn.blobFromImage(self.cv_image, 1/float(255), (self.inpWidth, self.inpHeight), [0,0,0], 1, crop=False)
         # Sets the input to the network
-        # rospy.loginfo('yolo_detector 2')
         self.net.setInput(blob)
 
         # Runs the forward pass to get output of the output layers
-        # rospy.loginfo('yolo_detector 3')
         outs = self.net.forward(self.getOutputsNames(self.net))
 
         # Remove the bounding boxes with low confidence
-        # rospy.loginfo('yolo_detector 4')
         self.postprocess(outs)
 
         # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-        # t, _ = self.net.getPerfProfile()
-        # label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
-        # cv2.putText(self.cv_image, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+        t, _ = self.net.getPerfProfile()
+        label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
+        cv2.putText(self.cv_image, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
 
+        # uncomment if action should show detection image directly
         # cv2.imshow(self.winName, self.cv_image)
         # cv2.waitKey(3)
 
         try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(self.cv_image, "bgr8"))
             self.boundingBoxes.header.stamp = rospy.Time.now()
             self.boundingBoxes.header.frame_id = 'header_frame_id'
             return self.boundingBoxes
@@ -208,54 +190,23 @@ class yolo_action(object):
 
     def BBcheck_cb(self, goal):
         success = False
-        # self._result.targetBB = [None]*4
         self._result.targetBB = None
-        # rospy.loginfo('detection started')
-
         self._feedback.in_progress = True
-        # rospy.loginfo('BBcheck_cb 1')
-        
         interm_result = self.yolo_detector()
         self._feedback.detectedBB = interm_result
-        # rospy.loginfo('BBcheck_cb 2')
-        # rospy.loginfo('detection finished, %s objects present',len(interm_result.bounding_box))
-        # for box in interm_result.bounding_box:
-            # rospy.loginfo('  - %s',box.Class)
-        # for item in interm_result.bounding_box:
-        #     rospy.loginfo('  - %s',item.Class)
-        # rospy.loginfo('start looking for %s' ,goal.name)
-        # rospy.loginfo('start looking for orange")
-        # rospy.loginfo('BBcheck_cb 4')
-        i = 1
-        # for box in self._feedback.detectedBB.bounding_box:
-        # rospy.loginfo(goal)
+        
         for box in interm_result.bounding_box:
-            # if box.Class == goal.name: # and self.request:
-            if box.Class == "orange": # and self.request:
+            if box.Class == "orange":
                 self._result.targetBB = box
-                # rospy.loginfo(box)
-                # rospy.loginfo(type(box))
-                # self._result.probability = box.probability
-                # self._result.targetBB[0] = box.xmin
-                # self._result.targetBB[1] = box.ymin
-                # self._result.targetBB[2] = box.xmax
-                # self._result.targetBB[3] = box.ymax
                 self._result.success = True
                 success = True
                 self._feedback.in_progress = False
-                i += 1
-        # rospy.loginfo('point 5')
 
         if success:
             self._as.set_succeeded(self._result)
-            # rospy.loginfo("object %s %s found in this frame with prob. %s" ,i ,goal,self._result.targetBB.probability)
-            # rospy.loginfo("BoundingBox: [%d,%d,%d,%d] " ,self._result.targetBB[0],self._result.targetBB[1],self._result.targetBB[2],self._result.targetBB[3])
-            # rospy.loginfo("BoundingBox: {0}" ,self._result.targetBB.xmin)
             success = False
         else:
             self._result.success = False
-            # rospy.loginfo("object not found in this frame")
-
         print("-")*10
 
 
