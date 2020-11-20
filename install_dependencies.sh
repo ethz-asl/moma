4ffe3613bf97dccf91c607348347f38338d7514d
Julian Förster <julian.foerster@mavt.ethz.ch>
2020-11-20 17:42:04 +0100

    Updated ridgeback submodule and added ones that are not available under noetic

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,8 +3,8 @@ sudo apt-get install \
 	ros-$ROS_DISTRO-ros-controllers \
 	ros-$ROS_DISTRO-gazebo-ros-pkgs \
 	ros-$ROS_DISTRO-gazebo-ros-control \
-	ros-$ROS_DISTRO-ridgeback-desktop \
-	ros-$ROS_DISTRO-ridgeback-simulator \
+	# ros-$ROS_DISTRO-ridgeback-desktop \
+	# ros-$ROS_DISTRO-ridgeback-simulator \
 	ros-$ROS_DISTRO-moveit \
 	ros-$ROS_DISTRO-rosmon \
 	ros-$ROS_DISTRO-pcl-ros \
