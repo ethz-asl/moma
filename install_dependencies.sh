890a5ef959faf0e4f4f4fb43d41485c35f1f0f78
Julian Förster <julian.foerster@mavt.ethz.ch>
2020-12-03 17:51:35 +0100

    .

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,6 +3,7 @@ sudo apt-get install \
 	ros-$ROS_DISTRO-ros-controllers \
 	ros-$ROS_DISTRO-gazebo-ros-pkgs \
 	ros-$ROS_DISTRO-gazebo-ros-control \
+	ros-$ROS_DISTRO-gazebo-ros \
 	ros-$ROS_DISTRO-ridgeback-desktop \
 	ros-$ROS_DISTRO-ridgeback-simulator \
 	ros-$ROS_DISTRO-moveit \
