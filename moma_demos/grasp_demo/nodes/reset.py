40fc81c7dc33b0d37566caf4fd280a8e8033b871
aslpanda <ethz.asl.panda@gmail.com>
2021-06-25 16:57:45 +0200

    Add collision box for table

--- moma_demos/grasp_demo/nodes/reset.py
+++ moma_demos/grasp_demo/nodes/reset.py
@@ -13,8 +13,18 @@ class ResetNode(object):
     def __init__(self):
         self.arm = PandaCommander()
         self.home_joints = rospy.get_param("moma_demo/home_joints_arm")
+        self.create_planning_scene()
         rospy.Service("reset", Trigger, self.reset)
         rospy.loginfo("Reset service ready")
+
+    def create_planning_scene(self):
+        # collision box for table
+        msg = geometry_msgs.msg.PoseStamped()
+        msg.header.frame_id = "panda_link0"
+        msg.pose.position.x = 0.4
+        msg.pose.position.y = 0.0
+        msg.pose.position.z = 0.06
+        self.arm.scene.add_box("table", msg, size=(0.6, 0.6, 0.02))
 
     def reset(self, req):
         rospy.loginfo("Reset")
