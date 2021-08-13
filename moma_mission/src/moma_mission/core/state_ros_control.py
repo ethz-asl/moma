68b494d1b7ad32503daa1f4ec5b76bb9ee07d0ff
aslpanda <ethz.asl.panda@gmail.com>
2021-08-13 16:44:30 +0200

    working state machine on hardware (no interaction)

--- moma_mission/src/moma_mission/core/state_ros_control.py
+++ moma_mission/src/moma_mission/core/state_ros_control.py
@@ -1,3 +1,4 @@
+import rospy
 from moma_mission.core import StateRos
 from moma_mission.utils.ros import switch_ros_controller
 
@@ -14,7 +15,14 @@ class StateRosControl(StateRos):
         self.manager_namespace = self.get_scoped_param("manager_namespace")
         self.whitelist = self.get_scoped_param("whitelist")
 
-    def do_switch(self):
-        return switch_ros_controller(controller_name=self.controller_name,
-                                     manager_namespace=self.manager_namespace,
-                                     whitelist=self.whitelist)
+    def do_switch(self, wait_before_return=3.0):
+        """ wait before return is to avoid that what executes next is not catched by the controller"""
+        success = switch_ros_controller(controller_name=self.controller_name,
+                                        manager_namespace=self.manager_namespace,
+                                        whitelist=self.whitelist)
+        if not success:
+            return success
+
+        rospy.loginfo("Waiting {} sec before returning after controller switch".format(wait_before_return))
+        rospy.sleep(wait_before_return)
+        return success
