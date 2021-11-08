78b17cacb27ffbf8169e28dd65581b89ddfc2e38
Julian Keller <ethz@juliankeller.net>
2021-11-08 15:38:40 +0100

    Add prototype of position control sliders

--- moma_tools/rqt_position_setpoint/src/rqt_position_setpoint/position_setpoint.py
+++ moma_tools/rqt_position_setpoint/src/rqt_position_setpoint/position_setpoint.py
@@ -8,7 +8,7 @@ import yaml
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from python_qt_binding.QtWidgets import QWidget, QHBoxLayout, QPushButton, QSlider
 from python_qt_binding.QtCore import Qt, QTimer, QSize
 from python_qt_binding.QtGui import QStandardItemModel, QStandardItem
 
@@ -16,13 +16,22 @@ from controller_manager_msgs.utils import ControllerLister
 
 from sensor_msgs.msg import JointState
 
-class CustomWidget(QWidget):
+class PresetWidget(QWidget):
     def __init__(self, parent=None):
-        super(CustomWidget, self).__init__(parent)
+        super(PresetWidget, self).__init__(parent)
         self.button = QPushButton('  Go to Position  ')
         self.button.setFixedHeight(30)
         lay = QHBoxLayout(self)
         lay.addWidget(self.button, alignment=Qt.AlignRight)
+        lay.setContentsMargins(0, 0, 0, 0)
+
+class ControlWidget(QWidget):
+    def __init__(self, parent=None):
+        super(ControlWidget, self).__init__(parent)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setFixedHeight(30)
+        lay = QHBoxLayout(self)
+        lay.addWidget(self.slider, alignment=Qt.AlignRight)
         lay.setContentsMargins(0, 0, 0, 0)
 
 class PositionSetpoint(Plugin):
@@ -51,15 +60,26 @@ class PositionSetpoint(Plugin):
         self.presets = rosparam.load_file(os.path.join(pkg_dir, 'config', 'presets.yaml'))[0][0]
 
         preset_view = self._widget.preset_view
-        model = QStandardItemModel(preset_view)
-        preset_view.setModel(model)
+        preset_model = QStandardItemModel(preset_view)
+        preset_view.setModel(preset_model)
         for name, preset in self.presets.items():
             item = QStandardItem(name)
             item.setSizeHint(QSize(0, 30))
-            model.appendRow(item)
-            widget = CustomWidget(parent=self._widget)
+            preset_model.appendRow(item)
+            widget = PresetWidget(parent=self._widget)
             widget.button.clicked.connect((lambda pr: lambda: self._on_preset(pr))(preset))
             preset_view.setIndexWidget(item.index(), widget)
+
+        control_view = self._widget.control_view
+        control_model = QStandardItemModel(control_view)
+        control_view.setModel(control_model)
+        for joint_name in sorted(self.presets.values()[0]['joint_positions'].keys()):
+            item = QStandardItem(joint_name)
+            item.setSizeHint(QSize(0, 30))
+            control_model.appendRow(item)
+            widget = ControlWidget(parent=self._widget)
+            control_view.setIndexWidget(item.index(), widget)
+        self._widget.send_all.clicked.connect(self._on_command)
 
         self.pub_goal = rospy.Publisher('/joint_space_controller/goal', JointState, queue_size=1)
 
@@ -78,6 +98,16 @@ class PositionSetpoint(Plugin):
         for name, position in sorted(preset['joint_positions'].items()):
             #goal.name.append(name)
             goal.position.append(position)
+        self.pub_goal.publish(goal)
+
+    def _on_command(self):
+        goal = JointState()
+        control_view = self._widget.control_view
+        model = control_view.model()
+        for i in range(model.rowCount()):
+            item = model.item(i)
+            widget = control_view.indexWidget(item.index())
+            goal.position.append(widget.slider.value())
         self.pub_goal.publish(goal)
 
     def _update_controllers(self):
