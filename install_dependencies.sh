1a51c6d5aeb1bd3f94088bdaa75b4b88443a4968
smb261-panda <grizzi@ethz.ch>
2022-01-31 11:44:22 +0100

    Add pinocchio python bindings build

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -57,7 +57,7 @@ install_pinocchio() {
       mkdir build
       cd build
 
-      cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PINOCCHIO_INSTALL_PREFIX} -DBUILD_WITH_COLLISION_SUPPORT=ON -DBUILD_PYTHON_INTERFACE=OFF -DBUILD_TESTING=OFF || fail "Please resource ~/.moma_bashrc and restart the script"
+      cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PINOCCHIO_INSTALL_PREFIX} -DBUILD_WITH_COLLISION_SUPPORT=ON -DBUILD_PYTHON_INTERFACE=ON -DBUILD_TESTING=OFF || fail "Please resource ~/.moma_bashrc and restart the script"
       make -j4 || fail "Error building pinocchio"
 
       mkdir install
@@ -71,7 +71,7 @@ install_pinocchio() {
 export PATH=${PINOCCHIO_INSTALL_PREFIX}/bin:\$PATH
 export PKG_CONFIG_PATH=${PINOCCHIO_INSTALL_PREFIX_STR}/lib/pkgconfig:\$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=${PINOCCHIO_INSTALL_PREFIX_STR}/lib:\$LD_LIBRARY_PATH
-export PYTHONPATH=\$PYTHONPATH:${PINOCCHIO_INSTALL_PREFIX_STR}/lib/python2.7/dist-packages
+export PYTHONPATH=\$PYTHONPATH:${PINOCCHIO_INSTALL_PREFIX_STR}/lib/python2.7/dist-packages:${PINOCCHIO_INSTALL_PREFIX_STR}/lib/python3/dist-packages
 export CMAKE_PREFIX_PATH=${PINOCCHIO_INSTALL_PREFIX_STR}:\$CMAKE_PREFIX_PATH
 EOF
 }
