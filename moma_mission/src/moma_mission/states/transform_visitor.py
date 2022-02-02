97621d15115d969ff7a1a22979ac6026c1332e59
Julian Keller <ethz@juliankeller.net>
2022-02-02 13:56:45 +0100

    Add end-effector flipping option avoiding cumbersome rotation

--- moma_mission/src/moma_mission/states/transform_visitor.py
+++ moma_mission/src/moma_mission/states/transform_visitor.py
@@ -1,7 +1,7 @@
 import rospy
+import numpy as np
 import tf
 import pinocchio as pin
-import rospy
 
 from nav_msgs.msg import Path
 
@@ -19,6 +19,7 @@ class TransformVisitorState(StateRosControl):
         self.angle_z = self.get_scoped_param("angle_z", 0)
         self.duration = self.get_scoped_param("duration", 0.0)
         self.timeout = self.get_scoped_param("timeout", max(30.0, 2 * self.duration))
+        self.allow_flip = self.get_scoped_param("allow_flip", False)
 
         self.path_publisher = rospy.Publisher(
             self.get_scoped_param("path_topic", "/desired_path"), Path, queue_size=1)
@@ -40,9 +41,18 @@ class TransformVisitorState(StateRosControl):
             path.poses.append(pose_stamped)
 
         T_w_t = self.get_transform(self.world_frame, self.target_frame)
-        H_t_toff = tf.transformations.rotation_matrix(self.angle_z, [0,0,1])
+        H_t_toff = tf.transformations.rotation_matrix(self.angle_z, [0, 0, 1])
         H_t_toff[0:3, 3] = self.offset
         H_w_toff = T_w_t.homogeneous @ H_t_toff
+        # Allow the target pose to be flipped by 180° around z axis
+        # if that avoids cumbersome gripper rotation
+        if self.allow_flip:
+            T_w_ee = self.get_transform(self.world_frame, self.ee_frame)
+            H_w_ee = T_w_ee.homogeneous
+            if np.dot(H_w_toff[0:3, 0], H_w_ee[0:3, 0]) < 0:
+                rospy.loginfo('Using a flipped pose')
+                H_toff_toffflip = tf.transformations.rotation_matrix(np.pi, [0, 0, 1])
+                H_w_toff = H_w_toff @ H_toff_toffflip
         T_w_toff = pin.SE3(H_w_toff)
 
         pose_stamped = se3_to_pose_stamped(T_w_toff, self.world_frame)
