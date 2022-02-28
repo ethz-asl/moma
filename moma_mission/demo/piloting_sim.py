a169316a33ff56a56a4dc865da946cb85cae578f
Julian Keller <ethz@juliankeller.net>
2022-02-28 17:08:38 +0100

    Add path visitor state, which abstracts the actual path execution from poses generation

--- moma_mission/demo/piloting_sim.py
+++ moma_mission/demo/piloting_sim.py
@@ -3,8 +3,9 @@
 import rospy
 import smach_ros
 from moma_mission.core import StateMachineRos
-from moma_mission.missions.piloting.manipulation_sim import ValveManipulationState
+from moma_mission.missions.piloting.manipulation import ValveManipulationUrdfState
 from moma_mission.states.gripper import GripperControl
+from moma_mission.states.path_visitor import PathVisitorState
 
 rospy.init_node('piloting_sim')
 
@@ -12,17 +13,29 @@ rospy.init_node('piloting_sim')
 # Build the state machine
 state_machine = StateMachineRos(outcomes=['Success', 'Failure'])
 with state_machine:
-    state_machine.add('OPEN_GRIPPER', GripperControl, transitions={'Completed': 'APPROACH_VALVE',
+    state_machine.add('OPEN_GRIPPER', GripperControl, transitions={'Completed': 'MODEL_VALVE',
                                                                    'Failure': 'Failure'})
 
-    state_machine.add('APPROACH_VALVE', ValveManipulationState, transitions={'Completed': 'CLOSE_GRIPPER',
-                                                                             'Failure': 'Failure'})
+    state_machine.add('MODEL_VALVE', ValveManipulationUrdfState, transitions={'Completed': 'APPROACH_VALVE',
+                                                                              'Failure': 'Failure'})
+
+    state_machine.add('APPROACH_VALVE', PathVisitorState, transitions={'Completed': 'GRASP_VALVE',
+                                                                       'Failure': 'Failure'})
+
+    state_machine.add('GRASP_VALVE', PathVisitorState, transitions={'Completed': 'CLOSE_GRIPPER',
+                                                                       'Failure': 'Failure'})
 
     state_machine.add('CLOSE_GRIPPER', GripperControl, transitions={'Completed': 'MANIPULATE_VALVE',
                                                                     'Failure': 'MANIPULATE_VALVE'})
 
-    state_machine.add('MANIPULATE_VALVE', ValveManipulationState, transitions={'Completed': 'Success',
-                                                                               'Failure': 'Failure'})
+    state_machine.add('MANIPULATE_VALVE', PathVisitorState, transitions={'Completed': 'RELEASE_GRIPPER',
+                                                                         'Failure': 'Failure'})
+
+    state_machine.add('RELEASE_GRIPPER', GripperControl, transitions={'Completed': 'BACKOFF_VALVE',
+                                                                      'Failure': 'Failure'})
+
+    state_machine.add('BACKOFF_VALVE', PathVisitorState, transitions={'Completed': 'Success',
+                                                                         'Failure': 'Failure'})
 
 # Create and start the introspection server
 introspection_server = smach_ros.IntrospectionServer('piloting_sim_mission_server', state_machine, '/mission_planner')
