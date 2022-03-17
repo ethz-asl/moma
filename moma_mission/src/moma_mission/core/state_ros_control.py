789e2ad246b9f79662a2b8ee497861b51477257c
Giuseppe Rizzi <grizzi@mavt.ethz.ch>
2022-03-17 11:11:38 +0100

    test changes

--- moma_mission/src/moma_mission/core/state_ros_control.py
+++ moma_mission/src/moma_mission/core/state_ros_control.py
@@ -21,7 +21,7 @@ class StateRosControl(StateRos):
         #                                                                       self.startlist[0])
         #                                             if len(self.startlist) > 0 else "")  -> does not work if the first controller in the list does not have this!
         # self.ee_frame = rospy.get_param(self.ee_frame_param, None)
-        self.ee_frame = "tool_frame"   # TODO: dirty hack remove soon please
+        self.ee_frame = self.get_scoped_param("ee_frame", "tool_frame")
 
     def do_switch(self, wait_before_return=3.0):
         return switch_ros_controller(startlist=self.startlist,
