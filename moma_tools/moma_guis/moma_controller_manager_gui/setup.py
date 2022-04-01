b8e0caf8492ba75f288c5ae8b0dd99a981dd43c1
Giuseppe Rizzi <grizzi@mavt.ethz.ch>
2022-04-01 12:10:29 +0200

    formatted code

--- moma_tools/moma_guis/moma_controller_manager_gui/setup.py
+++ moma_tools/moma_guis/moma_controller_manager_gui/setup.py
@@ -4,5 +4,4 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=["moma_controller_manager_gui"], package_dir={"": "src"}
 )
-
-  setup(**d)
+setup(**d)
