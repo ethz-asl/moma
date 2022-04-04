24c7aa91488c33e32a65ddaed05b40507583a0f1
Julian Keller <ethz@juliankeller.net>
2022-04-04 10:46:34 +0200

    Fix linting

--- moma_gazebo/scripts/grasp_action_relay.py
+++ moma_gazebo/scripts/grasp_action_relay.py
@@ -6,13 +6,18 @@ from actionlib.simple_action_server import SimpleActionServer
 from franka_gripper.msg import GraspAction, GraspGoal, GraspResult
 from control_msgs.msg import GripperCommandAction, GripperCommandGoal
 
+
 class GraspRelay:
     def __init__(self) -> None:
         self.in_action_name = rospy.get_param("~in_action_name")
         self.out_action_name = rospy.get_param("~out_action_name")
 
-        self.grasp_server = SimpleActionServer(self.in_action_name, GraspAction, self.relay_action, auto_start=False)
-        self.gripper_client = SimpleActionClient(self.out_action_name, GripperCommandAction)
+        self.grasp_server = SimpleActionServer(
+            self.in_action_name, GraspAction, self.relay_action, auto_start=False
+        )
+        self.gripper_client = SimpleActionClient(
+            self.out_action_name, GripperCommandAction
+        )
         self.grasp_server.start()
 
     def relay_action(self, goal: GraspGoal):
@@ -27,15 +32,22 @@ class GraspRelay:
             grasp_result.success = False
             grasp_result.error = f"Timeout while waiting for {self.out_action_name}"
 
-        if not self.gripper_client.send_goal_and_wait(gripper_goal, rospy.Duration(10.0)):
-            rospy.logerr(f"Timeout while waiting for result from {self.out_action_name}")
+        if not self.gripper_client.send_goal_and_wait(
+            gripper_goal, rospy.Duration(10.0)
+        ):
+            rospy.logerr(
+                f"Timeout while waiting for result from {self.out_action_name}"
+            )
             grasp_result.success = False
-            grasp_result.error = f"Timeout while waiting for result from{self.out_action_name}"
+            grasp_result.error = (
+                f"Timeout while waiting for result from{self.out_action_name}"
+            )
 
         if grasp_result.success:
             self.grasp_server.set_succeeded(grasp_result)
         else:
             self.grasp_server.set_aborted(grasp_result)
+
 
 if __name__ == "__main__":
     rospy.init_node("grasp_action_relay")
