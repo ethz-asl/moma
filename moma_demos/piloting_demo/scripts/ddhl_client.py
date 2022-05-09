3c338a5d052104ab1effd731b97cce1fb4ac766d
Julian Keller <ethz@juliankeller.net>
2022-05-09 15:52:42 +0200

    Fix shebang

--- moma_demos/piloting_demo/scripts/ddhl_client.py
+++ moma_demos/piloting_demo/scripts/ddhl_client.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
+
 import os
 
-import prompt_toolkit
 import rospy
 import requests
 import argparse
--- moma_demos/piloting_demo/scripts/report_generation.py
+++ moma_demos/piloting_demo/scripts/report_generation.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#!/usr/bin/python3
 
 from cmath import inf
 import os
