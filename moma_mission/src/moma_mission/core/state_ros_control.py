1d6e01b2be6b155f083803bf2fc2fabb44935fd0
Julian Keller <ethz@juliankeller.net>
2022-05-20 12:50:15 +0200

    Add additional start and stop maps for controlling different manager namespaces

--- moma_mission/src/moma_mission/core/state_ros_control.py
+++ moma_mission/src/moma_mission/core/state_ros_control.py
@@ -11,8 +11,12 @@ class StateRosControl(StateRos):
     def __init__(self, ns, outcomes=["Completed", "Failure"]):
         StateRos.__init__(self, ns=ns, outcomes=outcomes)
 
+        # The lists are assumed to be in the same namespace as the "manager_namespace"
         self.startlist = self.get_scoped_param("startlist", [])
         self.stoplist = self.get_scoped_param("stoplist", [])
+        # The maps are assumed to have the namespaces as keys
+        self.startmap = self.get_scoped_param("startmap", {})
+        self.stopmap = self.get_scoped_param("stopmap", {})
         self.manager_namespace = self.get_scoped_param("manager_namespace", "")
         self.ee_frame_param = self.get_scoped_param(
             "ee_frame_param",
@@ -22,7 +26,16 @@ class StateRosControl(StateRos):
         )
         self.ee_frame = rospy.get_param(self.ee_frame_param, None)
 
-    def do_switch(self, wait_before_return=3.0):
+    def do_switch(self):
+        manager_namespaces = set(list(self.startmap.keys()) + list(self.stopmap.keys()))
+        for ns in manager_namespaces:
+            switch_result = switch_ros_controller(
+                startlist=self.startmap[ns] if ns in self.startmap else [],
+                stoplist=self.stopmap[ns] if ns in self.stopmap else [],
+                manager_namespace=ns,
+            )
+            if not switch_result:
+                return switch_result
         return switch_ros_controller(
             startlist=self.startlist,
             stoplist=self.stoplist,
