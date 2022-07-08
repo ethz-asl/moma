9923038c1f081c7d94ddb48ed52dae673acbc1f9
matiov <matteo.iovino93@gmail.com>
2022-07-08 09:58:57 +0200

    Add some skill templates

--- moma_demos/mobile_manip_demo/scripts/behaviors.py
+++ moma_demos/mobile_manip_demo/scripts/behaviors.py
@@ -2,43 +2,68 @@
 
 from typing import Any, List
 
-from mobile_manip_demo import robot_interface
+import mobile_manip_demo.robot_interface as skills
 import py_trees as pt
 
 
 # import the robot interface where ROS bindings are defined
+# TODO: have a Node superclass that gets a State argument to track robot internal states
 
 
-class AtPos(pt.behaviour.Behaviour):
-    """Check if object is at position."""
+class RobotAtPose(pt.behaviour.Behaviour):
+    """Check if robot is at position."""
 
-    def __init__(
-        self,
-        name: str,
-        object_and_pos: list,
-        world_interface: Any,
-        verbose: bool = False,
-    ):
-        self.object = int(object_and_pos[0])
-        self.pos = sim.Pos(
-            object_and_pos[1][0], object_and_pos[1][1], object_and_pos[1][2]
-        )
-        super().__init__(name, world_interface, verbose)
+    def __init__(self, name: str, pose_ID: str):
+        super().__init__(name)
+        self.target_pose = get_pose_from_obj(pose_ID)
+        self.tolerance = get_navigation_tolerance()
+        self.interface = skills.RobotAtPose()
 
     def update(self):
-        if self.world_interface.is_close(self.object, self.pos):
-            self.success()
+        if self.interface.at_pose(self.target_pose, self.tolerance):
+            return pt.common.Status.SUCCESS
         else:
-            self.failure()
-        return self.state
+            return pt.common.Status.FAILURE
+
+
+class ObjectAtPose(pt.behaviour.Behaviour):
+    """Check if object is at position."""
+
+    def __init__(self, name: str, obj: str):
+        super().__init__(name)
+        self.target_object = obj
+        self.target_pose = get_pose_from_obj(obj)
+        self.tolerance = get_place_tolerance()
+        self.interface = skills.ObjectAtPose()
+
+    def update(self):
+        if self.interface.at_pose(self.target_pose, self.tolerance):
+            return pt.common.Status.SUCCESS
+        else:
+            return pt.common.Status.FAILURE
+
+
+class InHand(pt.behaviour.Behaviour):
+    """Check if object is held."""
+
+    def __init__(self, name: str, obj: str):
+        super().__init__(name)
+        self.target_object = obj
+        self.interface = skills.InHand()
+
+    def update(self):
+        if self.interface.in_hand(self.target_object):
+            return pt.common.Status.SUCCESS
+        else:
+            return pt.common.Status.FAILURE
 
 
 class Move(pt.behaviour.Behaviour):
     """Navigate to the input goal."""
 
-    def __init__(self, name: str, goal_ID: str, world_interface: Any):
+    def __init__(self, name: str, goal_ID: str):
         super().__init__(name)
-        self.interface = world_interface
+        self.interface = skills.Move()
         self.goal = goal_ID
 
     def initialise(self):
@@ -46,6 +71,56 @@ class Move(pt.behaviour.Behaviour):
 
     def update(self) -> pt.common.Status:
         status = self.interface.get_navigation_status()
+        if status == 0 or status == 1:
+            return pt.common.Status.RUNNING
+        elif status == 3:
+            return pt.common.Status.SUCCESS
+        else:
+            return pt.common.Status.FAILURE
+
+    def terminate(self, new_status: pt.common.Status):
+        if new_status == pt.common.Status.INVALID:
+            self.interface.cancel_goal()
+
+
+class Pick(pt.behaviour.Behaviour):
+    """Pick the target object."""
+
+    def __init__(self, name: str, goal_ID: str):
+        super().__init__(name)
+        self.interface = skills.Pick()
+        self.goal = goal_ID
+
+    def initialise(self):
+        self.interface.initialize_pick(self.goal)
+
+    def update(self) -> pt.common.Status:
+        status = self.interface.get_pick_status()
+        if status == 0 or status == 1:
+            return pt.common.Status.RUNNING
+        elif status == 3:
+            return pt.common.Status.SUCCESS
+        else:
+            return pt.common.Status.FAILURE
+
+    def terminate(self, new_status: pt.common.Status):
+        if new_status == pt.common.Status.INVALID:
+            self.interface.cancel_goal()
+
+
+class Place(pt.behaviour.Behaviour):
+    """Place the target object."""
+
+    def __init__(self, name: str, goal_ID: str):
+        super().__init__(name)
+        self.interface = skills.Place()
+        self.goal = goal_ID
+
+    def initialise(self):
+        self.interface.initialize_place(self.goal)
+
+    def update(self) -> pt.common.Status:
+        status = self.interface.get_place_status()
         if status == 0 or status == 1:
             return pt.common.Status.RUNNING
         elif status == 3:
--- moma_demos/mobile_manip_demo/scripts/robot_interface.py
+++ moma_demos/mobile_manip_demo/scripts/robot_interface.py
@@ -5,7 +5,6 @@ import rospy
 
 import numpy as np
 from numpy import linalg as LA
-import math
 
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty, SetBool, SetBoolRequest
@@ -16,20 +15,125 @@ from gazebo_msgs.msg import ModelStates
 
 # Action lib stuff
 from actionlib import SimpleActionClient
-from actionlib import simple_action_client
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from nav_msgs.msg import Odometry
-
-from moveit_msgs.msg import MoveItErrorCodes
-
-moveit_error_dict = {}
-for name in MoveItErrorCodes.__dict__.keys():
-    if not name[:1] == "_":
-        code = MoveItErrorCodes.__dict__[name]
-        moveit_error_dict[code] = name
 
 
-class MoveSkill:
+# CONDITIONS
+class RobotAtPose:
+    """Check that the robot is at the target position."""
+
+    def __init__(self, robot_name: str) -> None:
+        """Initialize ROS nodes."""
+        rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.amcl_pose_cb)
+        self.current_pos = None
+        # robot pose ground truth
+        self.robot_name = robot_name
+        self.ground_truth = None
+        rospy.Subscriber("gazebo/model_states", ModelStates, self.robot_callback)
+
+    def amcl_pose_cb(self, msg) -> None:
+        self.current_pos = np.array(
+            [
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
+            ]
+        )
+
+    def robot_callback(self, msg):
+        if self.robot_name in msg.name:
+            # the index may vary (don't know why)
+            idx = 0
+            for idx in range(len(msg.name)):
+                if msg.name[idx] == self.robot_name:
+                    break
+                else:
+                    continue
+            self.ground_truth = np.array(
+                [
+                    msg.pose[idx].position.x,
+                    msg.pose[idx].position.y,
+                    msg.pose[idx].position.z,
+                ]
+            )
+        else:
+            self.ground_truth = np.array([100, 100, 0])
+
+    def at_pose(
+        self, target_pose: np.ndarray, tolerance: float, ground_truth: bool = False
+    ) -> bool:
+        """Check if the current pose is within tolerance from the target pose."""
+        # Use e.g. [x,y] if the target is 2D
+        reference = self.current_pos[: target_pose.size + 1]
+        if ground_truth:
+            reference = self.ground_truth[: target_pose.size + 1]
+        distance = LA.norm(reference - target_pose)
+        # TODO: check orientation?
+        if distance < tolerance:
+            return True
+        return False
+
+
+# TODO: check if this can be integrated in the relative one for the robot
+class ObjectAtPose:
+    """Check that the object is at the target position."""
+
+    def __init__(self, target_object: str):
+        """Initialize ROS nodes."""
+        # object pose ground truth
+        rospy.Subscriber("gazebo/model_states", ModelStates, self.obj_callback)
+        self.target_obj = target_object
+        self.current_pos = None
+        self.ground_truth = None
+
+    def obj_callback(self, msg):
+        if self.target_obj in msg.name:
+            # the index may vary (don't know why)
+            idx = 0
+            for idx in range(len(msg.name)):
+                if msg.name[idx] == self.target_obj:
+                    break
+                else:
+                    continue
+            self.ground_truth = np.array(
+                [
+                    msg.pose[idx].position.x,
+                    msg.pose[idx].position.y,
+                    msg.pose[idx].position.z,
+                ]
+            )
+        else:
+            self.ground_truth = np.array([100, 100, 0])
+
+    def at_pose(
+        self, target_pose: np.ndarray, tolerance: float, ground_truth: bool = False
+    ) -> bool:
+        """Check if the current pose is within tolerance from the target pose."""
+        # Use e.g. [x,y] if the target is 2D
+        reference = self.current_pos[: target_pose.size + 1]
+        if ground_truth:
+            reference = self.ground_truth[: target_pose.size + 1]
+        distance = LA.norm(reference - target_pose)
+        # TODO: check orientation?
+        if distance < tolerance:
+            return True
+        return False
+
+
+class InHand:
+    """Check if the object is hold in hand."""
+
+    def __init__(self):
+        """Initialize ROS nodes."""
+        # TODO: check the robot state and the gripper state:
+        # InHand = True if the gripper is closed but not fully
+
+    def in_hand(self, target_object: str) -> bool:
+        return True
+
+
+# ACTIONS
+class Move:
     """Low level implementation of a Navigation skill."""
 
     def __init__(self) -> None:
@@ -92,299 +196,79 @@ class MoveSkill:
         self.move_client.cancel_goal()
 
 
-class RobotInterface:
-    """Class to interface control policies with the environment."""
+class Pick:
+    """Low level implementation of a Picking skill."""
 
-    def __init__(self, namespace: str):
+    def __init__(self) -> None:
         """Initialize ROS nodes."""
+        self.pick_client = SimpleActionClient("pick_client", PickAction)
+        self.pick_client.wait_for_server(rospy.Duration(100))
 
-        self.ns = namespace
+    def initialize_pick(self, goal_ID: str) -> None:
+        """Move the robot to the target pose."""
+        # TODO: implement method
 
-        rospy.Subscriber(
-            self.ns + "/gazebo/model_states", ModelStates, self.cube_callback
-        )
-        # TODO: get this from config
-        self.robot_name = "panda"
-        rospy.Subscriber(
-            self.ns + "/gazebo/model_states", ModelStates, self.robot_callback
-        )
-        rospy.Subscriber(
-            self.ns + "/amcl_pose", PoseWithCovarianceStamped, self.amcl_pose_cb
-        )
+        # send the goal
+        self.pick_client.send_goal(goal_ID)
 
-        # localization
-        self.cmd_vel_top = "/key_vel"
-        self.cmd_vel_pub = rospy.Publisher(self.cmd_vel_top, Twist, queue_size=10)
-        # command
-        self.move_msg = Twist()
-        self.move_msg.linear.x = 0
-        self.move_msg.angular.z = -1
-        # subscriber
-        rospy.Subscriber("/floating_base_pose_simulated", Odometry, self.odometry_cb)
-        rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.amcl_pose_cb)
-        # localisation services
-        glb_loc_srv = rospy.get_param(
-            "/robotics_intro/logic_state_machine/global_loc_srv", ""
-        )
-        rospy.wait_for_service(glb_loc_srv, timeout=30)
-        self.amcl_loc_srv = rospy.ServiceProxy(glb_loc_srv, Empty)
-        clr_cm_srv = rospy.get_param(
-            "/robotics_intro/logic_state_machine/clear_costmaps_srv", ""
-        )
-        rospy.wait_for_service(clr_cm_srv, timeout=30)
-        self.mb_clr_cm_srv = rospy.ServiceProxy(clr_cm_srv, Empty)
-
-        # navigation services
-        self.ref_frame = rospy.get_param(
-            "/robotics_intro/logic_state_machine/move_base_frame", ""
-        )
-        # pick
-        self.pick_pose_top = rospy.get_param(
-            "/robotics_intro/logic_state_machine/pick_pose_topic", ""
-        )
-        self.pick_pose_received = False
-        self.pick_pose_goal = PoseStamped()
-        self.pick_subs = rospy.Subscriber(
-            self.pick_pose_top, PoseStamped, self.pick_pose_cb
-        )
-        # place
-        self.place_pose_top = rospy.get_param(
-            "/robotics_intro/logic_state_machine/place_pose_topic", ""
-        )
-        self.place_pose_received = False
-        self.place_pose_goal = PoseStamped()
-        self.place_subs = rospy.Subscriber(
-            self.place_pose_top, PoseStamped, self.place_pose_cb
-        )
-        # action - get_param returns move_base
-        self.move_client = SimpleActionClient("move_client", MoveBaseAction)
-        self.move_client.wait_for_server(rospy.Duration(100))
-
-        # pick services
-        pick_srv = rospy.get_param("/robotics_intro/logic_state_machine/pick_srv", "")
-        rospy.wait_for_service(pick_srv, timeout=30)
-        self.pick_srv = rospy.ServiceProxy(pick_srv, SetBool)
-        # place services
-        place_srv = rospy.get_param("/robotics_intro/logic_state_machine/place_srv", "")
-        rospy.wait_for_service(place_srv, timeout=30)
-        self.place_srv = rospy.ServiceProxy(place_srv, SetBool)
-
-    # ROS related, e.g. callbacks
-    def assign_poses(self, pick_pose_, place_pose_):
-        # Pick pose
-        self.pick_pose.position.x = float(pick_pose_[0])
-        self.pick_pose.position.y = float(pick_pose_[1])
-        self.pick_pose.position.z = float(pick_pose_[2])
-        self.pick_pose.orientation.x = float(pick_pose_[3])
-        self.pick_pose.orientation.y = float(pick_pose_[4])
-        self.pick_pose.orientation.z = float(pick_pose_[5])
-        self.pick_pose.orientation.w = float(pick_pose_[6])
-
-        # Place pose
-        self.place_pose.position.x = float(place_pose_[0])
-        self.place_pose.position.y = float(place_pose_[1])
-        self.place_pose.position.z = float(place_pose_[2])
-        self.place_pose.orientation.x = float(place_pose_[3])
-        self.place_pose.orientation.y = float(place_pose_[4])
-        self.place_pose.orientation.z = float(place_pose_[5])
-        self.place_pose.orientation.w = float(place_pose_[6])
-
-    def robot_callback(self, msg):
-        if self.robot_name in msg.name:
-            # the index may vary (don't know why)
-            idx = 0
-            for idx in range(len(msg.name)):
-                if msg.name[idx] == self.robot_name:
-                    break
-                else:
-                    continue
-            # in the same way, also robot configuration can be retrieved
-            self.robot_pose.position.x = msg.pose[idx].position.x
-            self.robot_pose.position.y = msg.pose[idx].position.y
-            self.robot_pose.position.z = msg.pose[idx].position.z
-            self.robot_pose.orientation.x = msg.pose[idx].orientation.x
-            self.robot_pose.orientation.y = msg.pose[idx].orientation.y
-            self.robot_pose.orientation.z = msg.pose[idx].orientation.z
-            self.robot_pose.orientation.w = msg.pose[idx].orientation.w
-
-            self.robot_pose_list = []
-            self.robot_pose_list.append(msg.pose[idx].position.x)
-            self.robot_pose_list.append(msg.pose[idx].position.y)
-            self.robot_pose_list.append(msg.pose[idx].position.z)
-
-            error = np.fabs(LA.norm(self.robot_pose_list) - LA.norm(self.amcl_pose))
-            if error < 0.1:
-                self.localised = True
-            else:
-                self.localised = False
-
+    def get_pick_status(self) -> int:
+        """Get result from pick."""
+        wait = self.pick_client.wait_for_result(rospy.Duration(10))
+        if not wait:
+            rospy.logerr("Pick Action server not available!")
+            rospy.signal_shutdown("Pick Action server not available!")
+            return -1
         else:
-            self.robot_pose.position.x = 100.0
-            self.robot_pose.position.y = 100.0
-            self.robot_pose.position.z = 0.0
-            # orientation is not relevant
-            self.robot_pose.orientation.w = 1.0
+            # Result of executing the action
+            return self.pick_client.get_state()
 
-    def cube_callback(self, msg):
-        if "aruco_cube" in msg.name:
-            # the index may vary (don't know why)
-            idx = 0
-            for idx in range(len(msg.name)):
-                if msg.name[idx] == "aruco_cube":
-                    break
-                else:
-                    continue
-            # in the same way, also robot configuration can be retrieved
-            self.cube_pose.position.x = msg.pose[idx].position.x
-            self.cube_pose.position.y = msg.pose[idx].position.y
-            self.cube_pose.position.z = msg.pose[idx].position.z
-            # orientation is not relevant
-            self.cube_pose.orientation.w = 1.0
+    def cancel_goal(self) -> None:
+        """Cancel current pick goal."""
+        self.pick_client.cancel_goal()
 
-            self.cube_pose_list = []
-            self.cube_pose_list.append(msg.pose[idx].position.x)
-            self.cube_pose_list.append(msg.pose[idx].position.y)
-            self.cube_pose_list.append(msg.pose[idx].position.z)
 
-            error = np.fabs(
-                LA.norm(self.robot_pose_list[0:2]) - LA.norm(self.cube_pose_list[0:2])
-            )
-            if error > 0.8:
-                # rospy.sleep(2)
-                self.have_cube = False
+class Place:
+    """Low level implementation of a Placing skill."""
 
+    def __init__(self) -> None:
+        """Initialize ROS nodes."""
+        self.place_client = SimpleActionClient("place_client", PlaceAction)
+        self.place_client.wait_for_server(rospy.Duration(100))
+
+    def initialize_place(self, goal_ID: str) -> None:
+        """Move the robot to the target pose."""
+        # TODO: implement method
+
+        # send the goal
+        self.place_client.send_goal(goal_ID)
+
+    def get_place_status(self) -> int:
+        """Get result from place."""
+        wait = self.place_client.wait_for_result(rospy.Duration(10))
+        if not wait:
+            rospy.logerr("Pick Action server not available!")
+            rospy.signal_shutdown("Pick Action server not available!")
+            return -1
         else:
-            self.cube_pose.position.x = -100.0
-            self.cube_pose.position.y = -100.0
-            self.cube_pose.position.z = 0.0
-            # orientation is not relevant
-            self.cube_pose.orientation.w = 1.0
+            # Result of executing the action
+            return self.place_client.get_state()
 
-    def amcl_pose_cb(self, msg):
-        self.amcl_pose = []
-        self.amcl_pose.append(msg.pose.pose.position.x)
-        self.amcl_pose.append(msg.pose.pose.position.y)
-        self.amcl_pose.append(msg.pose.pose.position.z)
+    def cancel_goal(self) -> None:
+        """Cancel current pick goal."""
+        self.place_client.cancel_goal()
 
-    def odometry_cb(self, msg):
-        self.odometry = []
-        self.odometry.append(msg.pose.pose.position.x)
-        self.odometry.append(msg.pose.pose.position.y)
-        self.odometry.append(msg.pose.pose.position.z)
 
-    def pick_pose_cb(self, pose_msg):
-        self.pick_pose_goal = pose_msg
-        self.pick_pose_received = True
+# ROBOT STATE
+class State:
+    """Represent part of the robot's internal state that cannot be measured."""
 
-    def place_pose_cb(self, pose_msg):
-        self.place_pose_goal = pose_msg
-        self.place_pose_received = True
+    def __init__(self):
+        self.__holding = ""
 
-    # auxiliary stuff
-    def distance_pose(self, pose1, pose2):
-        d = Pose()
-        # orientation is not important
-        # this can be also transformed into a vector3 for simplicity
-        d.position.x = np.abs(pose1.position.x - pose2.position.x)
-        d.position.y = np.abs(pose1.position.y - pose2.position.y)
-        d.position.z = np.abs(pose1.position.z - pose2.position.z)
-        d.orientation.w = 1.0
+    @property
+    def holding(self):
+        return self.__holding
 
-        return d
-
-    def ready_to_pick(self):
-        d = self.distance_pose(self.robot_pose, self.cube_pose)
-        distance = math.sqrt(d.position.x**2 + d.position.y**2)
-        return distance < 1.0
-
-    def ready_to_place(self):
-        d = self.distance_pose(self.robot_pose, self.cube_goal)
-        distance = math.sqrt(d.position.x**2 + d.position.y**2)
-        return distance < 1.0
-
-    def pick(self):
-        """
-        Pickup the object.
-        """
-        success = False
-        if self.ready_to_pick():
-            # command
-            pick_srv_req = self.pick_srv(True)
-            done = False
-            while not done:
-                # if the request was succesful!
-                if pick_srv_req.success:
-                    done = True
-                    success = True
-                    self.arm_config = "pick"
-                # if it failed
-                elif not pick_srv_req.success:
-                    # rospy.logwarn('PICK ACTION FAILED')
-                    done = True
-                    success = False
-                # if it's still running
-                else:
-                    continue
-        else:
-            success = False
-
-        self.have_cube = success
-
-        return success
-
-    def place(self):
-        """
-        Place the object.
-        """
-        success = False
-        if self.ready_to_place():
-            place_srv_req = self.place_srv(True)
-            done = False
-            while not done:
-                # if the request was succesful!
-                if place_srv_req.success:
-                    done = True
-                    success = True
-                    self.arm_config = "place"
-                # if it failed
-                elif not place_srv_req.success:
-                    # rospy.logwarn('PICK ACTION FAILED')
-                    done = True
-                    success = False
-                # if it's still running
-                else:
-                    continue
-        else:
-            success = False
-
-        self.have_cube = not success
-
-        return success
-
-    def localise(self):
-        """
-        Conjoins spinning and localisation into one behaviour,
-        since the compounded approach failed without explanation...
-        """
-        amcl_loc_req = self.amcl_loc_srv()
-        # counter
-        i = 0
-        n = 400
-        # convergence checker
-        converged = False
-        while not converged and i <= n:
-            # increment counter
-            i += 1
-            error = np.fabs(LA.norm(self.odometry) - LA.norm(self.amcl_pose))
-            if error < 1e-2:
-                converged = True
-                mb_cc_req = self.mb_clr_cm_srv()
-            # keep spinning if still trying
-            else:
-                rate = rospy.Rate(10)
-                self.cmd_vel_pub.publish(self.move_msg)
-                rate.sleep()
-
-        self.localised = converged
-
-        return converged
+    @holding.setter
+    def holding(self, value):
+        self.__holding = value
