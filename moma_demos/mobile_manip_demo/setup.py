932332aeccd76f9442c4fcc36449101e92deb675
matiov <matteo.iovino93@gmail.com>
2022-07-27 17:10:58 +0200

    Fix indent

--- moma_demos/mobile_manip_demo/setup.py
+++ moma_demos/mobile_manip_demo/setup.py
@@ -4,6 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=["mobile_manip_demo"], package_dir={"": "src"})
+setup_args = generate_distutils_setup(
+    packages=["mobile_manip_demo"], package_dir={"": "src"}
+)
 
 setup(**setup_args)
