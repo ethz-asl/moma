70d45ce6d0d2d0baef824fbb1d1a29383deeb9cb
jk-ethz <ethz@juliankeller.net>
2022-08-18 17:29:22 +0200

    Add missing roslint dep for smb_path_planner

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -145,6 +145,7 @@ install_piloting() {
 install_system_deps() {
 sudo --preserve-env=DEBIAN_FRONTEND apt-get install \
   git git-lfs python3-pip python3-vcstool \
+  ros-$ROS_DISTRO-roslint \
 	ros-$ROS_DISTRO-ros-control \
 	ros-$ROS_DISTRO-ros-controllers \
 	ros-$ROS_DISTRO-gazebo-ros-pkgs \
