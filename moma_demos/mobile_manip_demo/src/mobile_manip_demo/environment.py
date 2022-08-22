cb5b38e857c3213118572909fc90b9896e1b89b5
matiov <matteo.iovino93@gmail.com>
2022-08-22 17:30:28 +0200

    Add new utility function

--- moma_demos/mobile_manip_demo/src/mobile_manip_demo/environment.py
+++ moma_demos/mobile_manip_demo/src/mobile_manip_demo/environment.py
@@ -3,7 +3,9 @@
 
 
 from argparse import ArgumentError
-from ast import mod
+import numpy as np
+
+from geometry_msgs.msg import Pose
 
 
 def get_item_by_marker(marker_id: int, model_type: str = "cubes"):
@@ -30,3 +32,24 @@ def get_item_by_marker(marker_id: int, model_type: str = "cubes"):
         raise ArgumentError(f"Model type {model_type} not implemented.")
 
     return (name, link)
+
+
+def get_closest_robot_target(marker_pose: np.ndarray) -> np.ndarray:
+    """Get the robot pose that is closest to the given marker pose."""
+    search_waypoints = [
+        np.array([-1.0, -1.0, 0, 0, 0, 1, 0]),
+        np.array([-1.0, 2.0, 0, 0, 0, 1, 0]),
+        np.array([0.5, 2.3, 0, 0, 0, 0.7071068, 0.7071068]),
+        np.array([1.8, 1.5, 0, 0, 0, 0, 1]),
+        np.array([1.8, -1.0, 0, 0, 0, 0, 1]),
+    ]
+
+    dist = 1e10
+    idx = 0
+    for i, possible in enumerate(search_waypoints):
+        current_dist = np.linalg.norm(possible[:2] - marker_pose[:2])
+        if current_dist < dist:
+            dist = current_dist
+            idx = i
+
+    return search_waypoints[idx]
