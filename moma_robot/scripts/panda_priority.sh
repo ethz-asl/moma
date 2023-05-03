b550effd73a31985c426134086438e5abee9d551
jk-ethz <ethz@juliankeller.net>
2023-05-03 16:22:57 +0200

    Fix priority setting in case of crashing control node

--- moma_robot/scripts/panda_priority.sh
+++ moma_robot/scripts/panda_priority.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-until pid=$(pgrep franka_control)
+while true
 do
-    sleep 1
-    echo "Panda control priority could not be set yet, retrying..."
-done
+    until pid=$(pgrep franka_control)
+    do
+        sleep 1
+        echo "Panda control priority could not be set yet, retrying..."
+    done
 
-sudo renice -20 -p $pid
+    if ! ps -o ni $pid | grep -q '\-20'; then
+        sudo renice -20 -p $pid
+    fi
+    sleep 1
+done
 
 exit 0
