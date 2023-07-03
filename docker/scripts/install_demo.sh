0f17c78a6afc0f77d63187c883606beeb552296e
ASL Panda <ethz.asl.panda@gmail.com>
2023-07-03 19:37:09 +0200

    Get demo docker to work.

--- docker/scripts/install_demo.sh
+++ docker/scripts/install_demo.sh
@@ -3,10 +3,27 @@ set -o pipefail
 
 cd $MOMA_DEP_WS/src || exit 1
 
-# Install the MOMA repo
+# Install the MOMA, VGN, and ROBOT_HELPERS repos.
+vcs import --recursive --input $SCRIPTS_PATH/moma_demo.repos
 
+# Upgrade numpy version
+pip install --upgrade numpy==1.23.0
 
-# Install the VGN repo
+# Pip install VGN requirements
+pip install -r vgn/requirements.txt
 
+# Pip install gdown to get google drive files
+pip install gdown
 
-# Install the VGN assets
+# Make sure we can unzip too
+apt-get install unzip
+
+# Download the data file from google drive
+gdown 1MysYHve3ooWiLq12b58Nm8FWiFBMH-bJ
+
+# Unzip and place it in the correct place
+unzip data.zip -d vgn_data
+mv vgn_data/data/* vgn/assets/
+rm -r vgn_data
+
+# Hope that's it! :)
