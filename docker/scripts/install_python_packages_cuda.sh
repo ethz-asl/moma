1f69d341d9886e93c2f13b7314a345ad3592371d
Casimir-Feldmann <cfeldmann@student.ethz.ch>
2024-01-29 11:18:12 +0100

    fix missing sift import issue

--- docker/scripts/install_python_packages_cuda.sh
+++ docker/scripts/install_python_packages_cuda.sh
@@ -6,6 +6,7 @@ sudo apt-get install python3-tk -y
 pip3 install torch torchvision torchaudio
 pip3 install pyransac3d imageio einops scikit-learn scikit-image tqdm torchmetrics lightgbm
 pip3 install git+https://github.com/facebookresearch/segment-anything.git
+pip3 install opencv-contrib-python
 
 # Clear cache to keep layer size down
 rm -rf /var/lib/apt/lists/*
