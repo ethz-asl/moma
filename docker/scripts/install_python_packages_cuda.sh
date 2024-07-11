28465ca4c5c7041955bca604fb3482108722024a
Nikhilesh Alatur <nikhilesh.alatur@gmail.com>
2024-07-11 10:59:23 +0200

    Add RL OT packages to cuda docker.

--- docker/scripts/install_python_packages_cuda.sh
+++ docker/scripts/install_python_packages_cuda.sh
@@ -7,7 +7,7 @@ pip3 install torch torchvision torchaudio
 pip3 install pyransac3d imageio einops scikit-learn scikit-image tqdm torchmetrics lightgbm
 pip3 install git+https://github.com/facebookresearch/segment-anything.git
 pip3 install opencv-contrib-python
-
+pip3 install numba gymnasium[extra] POT tensorflow stable-baselines3[extra]
 # Clear cache to keep layer size down
 rm -rf /var/lib/apt/lists/*
 
