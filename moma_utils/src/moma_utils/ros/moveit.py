520ab944653cf89536211e52e631624d668977e4
Nikhilesh Alatur <nikhilesh.alatur@gmail.com>
2024-11-27 15:39:51 +0100

    moveit updated compute_cartesian_path

--- moma_utils/src/moma_utils/ros/moveit.py
+++ moma_utils/src/moma_utils/ros/moveit.py
@@ -45,7 +45,9 @@ class MoveItClient:
         waypoints = [to_pose_msg(target)]
         self.move_group.set_max_velocity_scaling_factor(velocity_scaling)
         self.move_group.set_max_acceleration_scaling_factor(acceleration_scaling)
-        plan, _ = self.move_group.compute_cartesian_path(waypoints, 0.01, 0.0)
+        # plan, _ = self.move_group.compute_cartesian_path(waypoints, 0.01, 0.0)
+        plan, _ = self.move_group.compute_cartesian_path(waypoints, 0.01, False)
+        
         return plan
 
 
