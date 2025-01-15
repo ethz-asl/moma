944e6af9ea92ff6e42f1f70d649062407983a905
Nikhilesh Alatur <nikhilesh.alatur@gmail.com>
2025-01-15 15:45:14 +0100

    Change name on button.

--- moma_ui/include/moma_ui/MomaPanel.h
+++ moma_ui/include/moma_ui/MomaPanel.h
@@ -112,7 +112,7 @@ protected:
     // MOVEIT
     // Qt elements
     QCheckBox* moveit_cmd_toggle_cmd_input_ = new QCheckBox("Pose/Joints");
-    QPushButton* moveit_cmd_execute_path_button_ = new QPushButton("Execute Path");
+    QPushButton* moveit_cmd_execute_path_button_ = new QPushButton("Follow Waypoints");
     QPushButton* moveit_reset_button_ = new QPushButton("Reset MoveIt");
     // LineEdit for offset tx, ty, tz
     QLineEdit* moveit_offset_tx_ = new QLineEdit;
