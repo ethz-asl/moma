b232d806b5b48c44f1ed221bb98fd6547bf939f9
Antonia <ahuefner@ethz.ch>
2025-05-25 15:46:22 +0000

    fix gotoL moveit function after changes in C++ api

--- moma_utils/src/moma_utils/ros/moveit.py
+++ moma_utils/src/moma_utils/ros/moveit.py
@@ -36,16 +36,16 @@ class MoveItClient:
 
         return plan
 
-    def gotoL(self, target, velocity_scaling=0.1, acceleration_scaling=0.1):
-        plan = self.planL(target, velocity_scaling, acceleration_scaling)
+    def gotoL(self, target, velocity_scaling=0.1, acceleration_scaling=0.1, eef_step=0.01):
+        plan = self.planL(target, velocity_scaling, acceleration_scaling, eef_step)
         success = self.execute(plan)
         return success
 
-    def planL(self, target, velocity_scaling=0.1, acceleration_scaling=0.1):
+    def planL(self, target, velocity_scaling=0.1, acceleration_scaling=0.1, eef_step=0.01):
         waypoints = [to_pose_msg(target)]
         self.move_group.set_max_velocity_scaling_factor(velocity_scaling)
         self.move_group.set_max_acceleration_scaling_factor(acceleration_scaling)
-        plan, _ = self.move_group.compute_cartesian_path(waypoints, 0.01, 0.0)
+        plan, _ = self.move_group.compute_cartesian_path(waypoints, eef_step, True)
         return plan
 
 
